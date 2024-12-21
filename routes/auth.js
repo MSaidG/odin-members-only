1897d8d7dedef74e1e9c05c3857a8aec25b325ae
Muhammed Said Gülberk <62105391+MSaidG@users.noreply.github.com>
2024-12-21 17:14:32 +0300

    Add sanitazion and validation to sign up form

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,10 @@ const express = require("express");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const pool = require("../db/pool");
+const { body, check, validationResult, ExpressValidator } = require("express-validator");
 
 const router = express.Router();
+
 
 passport.use(
   new LocalStrategy(async (username, password, done) => {
@@ -55,26 +57,101 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-router.post("/signup/password", async (req, res, next) => {
-  let { first_name, last_name, username, email, password } = req.body;
-  bcrypt.hash(password, 10, async (err, hash) => {
-    if (err) {
-      console.log(err);
-      return next(err);
+const alphaErr = "Must only contain alphabetical characters";
+const lengthErr = "Must be between 2 and 20 characters";
+const emailErr = "Must be a valid email address";
+const validateUser = [
+
+  body("first_name", "First Name").trim().escape()
+    .isAlpha().withMessage("First Name " + alphaErr)
+    .isLength({min: 2, max: 40}).withMessage("First Name " + lengthErr),
+  
+  body("last_name", "Last Name").trim().escape()
+    .isAlpha().withMessage("Last Name" + alphaErr)
+    .isLength({min: 2, max: 40}).withMessage("Last Name" + lengthErr),
+
+  body("email", "Email").trim()
+    .isEmail().withMessage(emailErr)
+    .custom( async (value) => {
+      const user = await pool.query("SELECT * FROM member WHERE email = $1", [
+        value
+      ])
+      if (user.id) {
+        throw new Error("Email already in use");
+      }
+    }),
+
+  body("username", "Username").trim().escape()
+    .custom( async (value) => {
+      const user = await pool.query("SELECT * FROM member WHERE username = $1", [
+        value
+      ])
+      if (user.id) {
+        throw new Error("Username already in use");
+      }
+    }),
+
+  body("password", "Password")
+    .isLength({ min: 5 }).withMessage('Password must be at least 5 chars long')
+    .matches(/\d/).withMessage('Password must contain a number')
+    .matches(/[a-z]/).withMessage('Password must contain a lower case letter')
+    .matches(/[A-Z]/).withMessage('Password must contain an upper case letter')
+    .matches(/[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/).withMessage('Password must contain a special character'),
+
+  body('confirm-password', 'Confirm Password').custom((value, { req }) => {
+    return value === req.body.password;
+  })
+
+
+  // body("confirm-password", "Confirm Password").custom((value, { req }) => {
+  //   if (value !== req.body.password) {
+  //     throw new Error("Passwords do not match");
+  //   }
+  // })
+
+]
+
+const signupPost = [
+  validateUser,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+
+      res.render("signup", { 
+        errors: errors.array(),
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        username: req.body.username,
+        email: req.body.email
+      });
+
+    } else {
+
+      let { first_name, last_name, username, email, password } = req.body;
+      bcrypt.hash(password, 10, async (err, hash) => {
+        if (err) {
+          console.log(err);
+          return next(err);
+        }
+        password= hash;
+        const client = await pool.connect();
+        try {
+          await client.query(
+            "INSERT INTO member (first_name, last_name, username, email, password, status) VALUES ($1, $2, $3, $4, $5, $6)",
+            [first_name, last_name, username, email, password, "basic"]
+          );
+          res.redirect("/");
+        } catch (err) {
+          return next(err);
+        }
+      });
+
     }
-    password= hash;
-    const client = await pool.connect();
-    try {
-      await client.query(
-        "INSERT INTO member (first_name, last_name, username, email, password, status) VALUES ($1, $2, $3, $4, $5, $6)",
-        [first_name, last_name, username, email, password, "basic"]
-      );
-      res.redirect("/");
-    } catch (err) {
-      return next(err);
-    }
-  });
-});
+
+  }
+]
+
+router.post("/signup/password", signupPost);
 
 router.post(
   "/login/password",
